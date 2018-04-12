9768a9c8c16817f70c4c3c1a1b9eeca4b4209503
Dan Sims <sims.daniel.ds@gmail.com>
2018-04-12 14:23:54 -0400

    collection view cell completed

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Dan Sims on 4/12/18.
+//  Copyright © 2018 Dan Sims. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
