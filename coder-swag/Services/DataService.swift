0dbb210fd472bb47878ce07aedd4c02d8710c41d
Dan Sims <sims.daniel.ds@gmail.com>
2018-04-12 14:08:28 -0400

    table views and cells done

---
+++ coder-swag/Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Dan Sims on 4/12/18.
+//  Copyright © 2018 Dan Sims. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
